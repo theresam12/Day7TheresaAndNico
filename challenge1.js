9ea813392b14e5f1a1db1e4800464701a174b024
theresam12 <learn@learn14.localdomain>
2018-01-30 16:39:16 -0800

    day 7 exercises

---
+++ challenge1.js
@@ -0,0 +1,8 @@
+function button1() {
+  document.getElementById("carouselSmallMistakes").style.display= "block"
+  document.getElementById("carouselBigMistakes").style.display="none"
+}
+function button2(){
+  document.getElementById("carouselBigMistakes").style.display="block"
+  document.getElementById("carouselSmallMistakes").style.display= "none"
+}
